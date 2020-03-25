85d30550566338efd3bf4aa5509d3b36c8de82ea
Matthew Ryan Blackburn <mrblackburn@wisc.edu>
2020-03-25 15:17:44 -0500

    updates to SASA script and accessory files

--- SASA.py
+++ SASA.py
@@ -32,6 +32,7 @@ import re                 # methods for string editing
 import decimal            # methods for correct rounding
 import csv                # methods for handling csv file i/o
 import time               # methods for tracking efficiency of the code (CPU time)
+import os                 # methods for directory handling
 
 ## Dictionary containing maximum sidechain SASA values (unrounded) for each biological amino acid; calculated with Pymol's get_area command, with dot_solvent = 1 (SASA = true)
 ## and with dot_density = 4 (maximum accuracy to the discrete surface area calculation); variables declared above methods are accessible to those methods! Key = residue name, value = SASA
@@ -117,7 +118,8 @@ def find_Allchain(seq, start):
         if presence == True:
             # using the reference residue side chain, calculate relative SASA by accessing from the dictionary of max SASA values.
             # Chain A must be specified every time, otherwise the value will be multiplied by the number of identical chains.
-            sasa = cmd.get_area("resi " + count + " and chain A", 1, 0)
+
+            sasa = cmd.get_area("resi " + count + " and sidechain" + " and chain A", 1, 0)
             rel_sasa = sasa / Max_SASA[ltr]
             burial = "Exposed"  # intialized arbitrarily to "Exposed"; if threshold minimum is met, does not update.
         
@@ -141,8 +143,6 @@ def find_Allchain(seq, start):
 ################################################################################################################################################################
 ## Method for getting each lysine position and using it to make a separate residue selection, and calculate and write SASA to output. (ONLY SPECIFIED RESIDUES)#
 ################################################################################################################################################################
-
-## seq: string input (amino acid sequence); ch: the amino acid letter of choice; thr: relative SASA cutoff value for determining exposure
 def find_res(seq, ch, start):
     print("Length of fasta string (characters) is " + str(num_aa))
 
@@ -151,22 +151,32 @@ def find_res(seq, ch, start):
             ## Typecasting count (index or aa position) to a string allows the script to use count in a PyMOL selection-expression.
             count = str(count)
             residue = "" + ltr + count
-            ## selection algebra for picking one specific residue at a time and performing operation(s) on it.
-            ## note that this specific line of code can be universally useful for making discrete selections.
-            cmd.select("sele", "resn " + ltr + " and resi " + count + " and chain A")
+            
+            # Method call: Qualify this selection is actually present in the structure model
+            presence = occupancy(count)
+            occu_vals.clear()
 
-            ## SASA and relative SASA are calculated for each residue <ch> in the string fasta and then typecast to string so it can be printed easily.
-            # using the reference residue side chain, calculate relative SASA by accessing from the dictionary of max SASA values.
-            sasa = cmd.get_area("sele", 1, 0)
-            rel_sasa = sasa / Max_SASA[ch]
-            # threshold check: exposure is "buried" or "exposed".
-            burial = "Exposed"  # intialized arbitrarily to "Exposed"; if threshold minimum is met, does not update.
-            
-            if rel_sasa <= threshold:
-                burial = "Buried"
-            
-            sasa = str(sasa)
-            rel_sasa = str(rel_sasa)
+            # Calculations are not performed if the electron density is missing from the model; instead, their values are
+            # defaulted to "Not present in structure model", "N/A", and "N/A"
+            if presence == True:
+                # using the reference residue side chain, calculate relative SASA by accessing from the dictionary of max SASA values.
+                # Chain A must be specified every time, otherwise the value will be multiplied by the number of identical chains.
+
+                sasa = cmd.get_area("resi " + count + " and sidechain" + " and chain A", 1, 0)
+                rel_sasa = sasa / Max_SASA[ltr]
+                burial = "Exposed"  # intialized arbitrarily to "Exposed"; if threshold minimum is met, does not update.
+        
+                if rel_sasa <= threshold:
+                    burial = "Buried"
+        
+                sasa = str(sasa)
+                rel_sasa = str(rel_sasa)
+        
+            else:
+                sasa = "Not present in structure model"
+                rel_sasa = "N/A"
+                burial = "N/A"
+    
             ## Each SASA printed to console and then to output
             print("\n" + residue + " | " + sasa + " | " + rel_sasa + " | " + burial)
             current_row = [residue, sasa, rel_sasa, burial]
@@ -179,6 +189,7 @@ def find_res(seq, ch, start):
 ## Query the user for job parameters:
 print("Welcome to SASAfrass. \nThis script automates the calculation the solvent-accessible surface area (SASA) of a target list of amino acid residue sidechains from a list of protein models.\nThe calculation uses PyMOL's built-in SASA algorithm, which requires that the protein has a structural model file that can be read by PyMOL.")
 #TODO print("Does your job contain more than one protein?") #if yes, provide the name of the list file or manually enter a list of PDB IDs or filenames
+#TODO purpose is to direct user to using the HTC cluster for jobs containing multiple proteins. More than one is too heavy for a personal machine.
 print("")
 
 jobquery = str(input("Enter the PDB ID of the protein model you would like to analyze, then press ENTER:\n"))
@@ -192,7 +203,7 @@ print("You entered " + jobquery1 + ".\n")
 ## "Stopwatch" starts now
 start_time = time.time()
 
-with open('SASA_' + jobquery + '.csv', 'w', newline = '') as file: # write output values into csv row by row, vals in separate columns
+with open('SASA_' + jobquery + '_' + jobquery1 + '.csv', 'w', newline = '') as file: # write output values into csv row by row, vals in separate columns
     writer = csv.writer(file, delimiter = ',')
     writer.writerow(header)
 
@@ -233,4 +244,4 @@ with open('SASA_' + jobquery + '.csv', 'w', newline = '') as file: # write outpu
 
 ## "Stopwatch" stops now; print runtime
 stop_time = time.time()
-print("Time (seconds) taken for SASA calculations: " + str(stop_time - start_time))
+print("\nTime (seconds) taken for SASA calculations: " + str(stop_time - start_time) + "\n\nOutput file: " + "SASA_" + jobquery + "_" + jobquery1 + ".csv was saved in the working directory: " + os.getcwd())
